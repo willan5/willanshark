f15d63ae58b6b7bae5c15a6d1d4977693cf7bf2f
Willan5 <kwanjehia@gmail.com>
2022-05-04 07:27:59 +0300

    initial commit

--- js/toggle.js
+++ js/toggle.js
@@ -1,32 +1,14 @@
-// let menu = document.querySelector('.menu');
-// let btn = menu.querySelector('.gg-menu');
-// btn.addEventListener('click', evt => {
-// 	menu.classList.toggle('active');
-// })
 function openNav(){
-//     // document.getElementById("mobile");
+
     document.getElementById("navmob").style.display="block";
     document.getElementById("opennav").style.display="none";
     document.getElementById("refnav").style.display="none";
     // document.getElementsById("iconmobile").style.display="none"; 
 
-
 }
 function closeNav(){
-    //     // document.getElementById("mobile");
         document.getElementById("navmob").style.display="none";
         document.getElementById("opennav").style.display="grid";
         document.getElementById("refnav").style.display="grid";
         // document.getElementsById("iconmobile").style.display="grid";
-    }
-// function openNav(){
-//   let y = document.getElementsByClassName("nav-toggle");
-// //   let x = document.getElementsByClassName("active");
-//     if (y.style.display === "none") {
-//         onclick.y.style.display = "block";    
-//     }
-//      else 
-//     {
-//     y.style.display = "none";
-//     }
-// }
+}
