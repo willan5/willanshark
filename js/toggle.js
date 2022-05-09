449483861de7d64481022079fc8311d8cd57bfbf
Willan5 <kwanjehia@gmail.com>
2022-05-09 16:06:37 +0300

    initial commit

--- js/toggle.js
+++ js/toggle.js
@@ -3,12 +3,16 @@ function openNav(){
     document.getElementById("navmob").style.display="block";
     document.getElementById("opennav").style.display="none";
     document.getElementById("refnav").style.display="none";
-    // document.getElementsById("iconmobile").style.display="none"; 
+    //document.getElementsById("iconmobile").style.display="none"; 
 
 }
 function closeNav(){
         document.getElementById("navmob").style.display="none";
         document.getElementById("opennav").style.display="grid";
         document.getElementById("refnav").style.display="grid";
-        // document.getElementsById("iconmobile").style.display="grid";
+      //  document.getElementsById("iconmobile").style.display="none";
+}
+function signs(){
+    let y=document.getElementById("addsub");
+    y.append('<div class="inc button">+</div><div class="dec button"')
 }
